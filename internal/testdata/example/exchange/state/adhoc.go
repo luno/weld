1ed2073ad7b81a288633e087bab0eee93f4b2fef
Ed <echarrod@users.noreply.github.com>
2024-10-25 11:46:31 +0100

    testdata: Fully qualify imports

--- internal/testdata/example/exchange/state/adhoc.go
+++ internal/testdata/example/exchange/state/adhoc.go
@@ -1,11 +1,11 @@
 package state
 
 import (
-	"example/exchange"
 	"fmt"
 
-	exchange_ops "example/exchange/ops"
 	"github.com/luno/weld"
+	"github.com/luno/weld/internal/testdata/example/exchange"
+	"github.com/luno/weld/internal/testdata/example/exchange/ops"
 )
 
 var ChanProvider = weld.NewSet(NewModelChan)
@@ -14,7 +14,7 @@ func NewModelChan() chan<- exchange.Model {
 	return make(chan<- exchange.Model)
 }
 
-func NewGenericStringType() exchange_ops.GenericStringType {
+func NewGenericStringType() ops.GenericStringType {
 	return func(a exchange.Model, b string) string {
 		return fmt.Sprintf("%s%s", a, b)
 	}
